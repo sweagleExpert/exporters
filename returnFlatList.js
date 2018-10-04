82d68254922f2c7be0307c1f4b7d08d64e12c427
Anastasia Gik <anastasiagik@gmail.com>
2018-10-04 15:38:13 +0300

    New parser

---
+++ returnFlatList.js
@@ -0,0 +1,30 @@
+var returnedObject = {};
+var mdsName = Object.keys(metadataset);
+if ( mdsName.length > 0 && metadataset.hasOwnProperty(mdsName[0]) && metadataset[mdsName[0]].hasOwnProperty(mdsName[0])) {
+
+    checkNestedObject( metadataset[mdsName[0]][ mdsName[0] ] , returnedObject );
+    
+}
+
+var includedKeys = Object.keys(metadataset[mdsName[0]]);
+
+for (var i = 0;i<includedKeys.length; i++) {
+    if (includedKeys[i] !== mdsName[0]) {
+        returnedObject[includedKeys[i]] = metadataset[mdsName[0]][includedKeys[i]];
+    }
+}
+
+return returnedObject;
+
+
+function checkNestedObject ( nestedObject , returnedObject ) {
+    var objKeys = Object.keys(nestedObject);
+    
+    for (var i = 0; i < objKeys.length; i++ ) {
+        if (typeof nestedObject [ objKeys[i] ] === 'object') {
+            checkNestedObject ( nestedObject [ objKeys[i] ] , returnedObject  );
+        }else {
+            returnedObject[ objKeys[i] ] = nestedObject [ objKeys[i] ] ;
+        }
+    }
+}

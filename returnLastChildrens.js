471fc246bf4b59214bffd7df18b3c662e6164958
Dimitris FINAS <dfinas@gmail.com>
2018-11-12 19:26:22 +0100

    new exporter for customer POC

---
+++ returnLastChildrens.js
@@ -0,0 +1,48 @@
+//
+// returnLastChildrens.js
+// Exporter to return the last nodes in a MetaDataSet
+// Last nodes are identified by the fact they have no childrens
+//
+// This exporter is useful if last nodes represents different files of a confiquration
+// You use first this exporter to get list of nodes=files,
+// Then you use retrieveAllDataFromNode to get the different configuration files
+//
+// No inputs except MDS to parse
+// Outputs are: Array of children nodes names
+//
+// Creator:   Anastasia and Dimitris for customer POC
+// Version:   1.0
+//
+var mds = metadataset;
+var nodesFound = [];
+
+// Check if provided subset has children
+function hasChildren(subset) {
+  for (var i in subset) {
+    if (typeof(subset[i]) === 'object') { return true; }
+  }
+}
+
+function checkChildrent( subset ) {
+    for (var item in subset) {
+        if (typeof(subset[item]) === 'object') {
+             if ( hasChildren(subset[item]) === true ) {
+                 checkChildrent( subset[item] );
+             }else {
+                 nodesFound.push(item);
+             }
+        }
+    }
+}
+
+for (var item in mds) {
+    if (typeof(mds[item]) === 'object') {
+         if ( hasChildren(mds[item]) === true ) {
+             checkChildrent( mds[item] );
+         }else {
+             nodesFound.push(item);
+         }
+    }
+}
+
+return nodesFound;

234c9a3f47218015e77d88c066249d3d6a289e59
Dimitris FINAS <dfinas@gmail.com>
2019-06-21 16:22:59 +0200

    add group size

--- returnNextIndex.js
+++ returnNextIndex.js
@@ -5,12 +5,13 @@
 // Inputs are
 //    - prefix of the pattern to search for
 //    - regex of pattern to search for or keyword of pattern
-//    keywords list are : ip for next ip address, index to search for next integer value,
-//    index[0-9] to search for next index with left padding with specified number of 0
-//    - (optional) offset to add to max value found for result calculation (default is 1)
+//    keywords list are : ip for next ip address, index to search for next integer
+//    - (optional) pad[0-9] = search for next index with left padding with specified number of 0
+//    - (optional) offset[0-9] = integer to add to max value found for result calculation (default is 1)
+//    - (optional) group[0-9] = number of values to return
 //
-// ex: if you want the next hostname following rule "hostprd000 hostprd010 horstprd020",
-// you will put args: hostprd index3 10 and function will return hostprd030
+// ex: if you want the next hostname following values in model "hostprd000 hostprd010  horstprd020",
+// you will put args: hostprd index pad3 offset10 and function will return hostprd030
 //
 // Creator:   Anastasia and Dimitris for customer POC
 // Version:   1.2 - add offset as optional argument
@@ -19,7 +20,9 @@
 // Default values
 var offset = 1;
 var padSize = 0;
+var groupSize = 1;
 var maxValue = "";
+var returnValues = [];
 var ipPattern="(([01]?[0-9]?[0-9])|(2[0-4][0-9])|(25[0-4]))"
 
 // Read input arguments
@@ -28,19 +31,25 @@ var pattern = args[1];
 if (! (args[0] && args[1])) {
   return "*** ERROR: NOT ENOUGH ARGUMENTS SUPPLIED: you must provide at least PREFIX and PATTERN ***";
 }
-if (args[2]) {
-    offset = parseInt(args[2]);
+for (var i=2; i<5; i++) {
+    if (args[i]) {
+        if (args[i].toLowerCase().includes("pad")) {
+            padSize = parseInt(args[i].substring(3));
+        } else if (args[i].toLowerCase().includes("group")) {
+            groupSize = parseInt(args[i].substring(5));
+        } else if (args[i].toLowerCase().includes("offset")) {
+            offset = parseInt(args[i].substring(6));
+        }
+    }
 }
 
-
+// Calculate the pattern to search for
 var fullPattern = new RegExp ("^"+ body + pattern + "$");
 if (pattern.toLowerCase() == "ip") {
     fullPattern = new RegExp ("^"+ body + ipPattern + "$");
-} else if (pattern.toLowerCase().includes("index")) {
-    // check if there is an index value
-    var indexValue = pattern.substring(5);
-    if (indexValue) {
-        padSize = parseInt(indexValue);
+} else if (pattern.toLowerCase() == "index") {
+    // check if there is an padding value
+    if (padSize > 0) {
         fullPattern = new RegExp ("^"+ body + "[0-9]".repeat(padSize) + "$");
     } else {
         // If no padding, just search for number after prefix
@@ -77,21 +86,25 @@ findObjectKeys(metadataset);
 
 if (maxValue !== "") {
   var finalMaxValue = maxValue.split(body);
-  finalMaxValue[1] = parseInt(finalMaxValue[1]) + offset;
-  if (padSize > 0) {
-    var returnValue = body + pad(finalMaxValue[1],padSize);
-  } else {
-    var returnValue = body + finalMaxValue[1];
-  }
-//console.log("returnValue="+returnValue);
-  if (fullPattern.test(returnValue)) {
-    return returnValue;
-  } else {
-    return "Out of bounds";
+  for (var i=1; i<groupSize+1; i++) {
+      finalMaxValue[1] = parseInt(finalMaxValue[1]) + offset;
+      if (padSize > 0) {
+        var returnValue = body + pad(finalMaxValue[1],padSize);
+      } else {
+        var returnValue = body + finalMaxValue[1];
+      }
+    //console.log("returnValue="+returnValue);
+      if (fullPattern.test(returnValue)) {
+        returnValues.push(returnValue);
+      } else {
+        returnValues.push("Out of bounds");
+      }
   }
 } else {
   return "Not Found";
 }
+
+return returnValues.join(', ')
 
 function pad(num, size) {
     var s = "000000000" + num;

743ee551823e95460c27d839fe311cc7a8b7bc09
dimitris_sweagle <dimitris@sweagle.com>
2020-05-12 12:38:11 +0200

    new 3.11 object arg

--- returnValueforKey.js
+++ returnValueforKey.js
@@ -8,47 +8,94 @@
 
 var searches = {};
 var keysWithSameName = 0;
+var keyname = "";
+var rootNode = "";
+var superCDS = {};
+// Errors variables
+var errorFound = false;
+var errors = [];
+var errors_description = '';
 
-/**
-* mds must be the given metadataset,
-* searchKey must be the string we want to check in the keys
-*/
-function findObjectKeys(mds, searchKey) {
+// HANDLERS
+// Inputs parser and checker
+if (args[0]!=null) {
+  // Input value in old notation (for retro compatibility)
+  keyname=args[0];
+} else if (arg!=null && arg!="") {
+  // Input values in object notation
+  // Checking the assigned config datasets and parse the node name from input values in object notation
+  keyname=objFormat(arg);
+} else {
+  errorFound=true;
+  errors.push("ERROR: No arg with key to search provided !");
+}
+for (var i=0; i<cds.length; i++){
+  rootNode = Object.keys(cds[i])[0];
+  superCDS[rootNode] = cds[i][rootNode];
+}
+
+if (keyname!=null && !errorFound) {
+  // call the function to find the keyName and return the value
+  findObjectKeys(superCDS, keyname);
+
+  // return the result or error message
+  if (keysWithSameName > 1) { return "ERROR: "+keysWithSameName+" keyNames ("+keyname+") found"; }
+  else if (keysWithSameName == 1) { return searches[keyname]; }
+  else  { return "ERROR: keyname ("+keyname+") not found"; }
+}
+
+
+errors_description = errors.join(', ');
+return {description: errors_description, result:!errorFound};
+
+
+// FUNCTIONS LIST
+// Parse the object notation: check upon against the RegEx format
+function objFormat(obj) {
+  var valueToCheck;
+  // <nodeName>Value</nodeName>
+  var xmlRegex='^\<.*\>(.*)<\/.*\>$';
+  // ---
+  //nodeName: Value
+  var yamlRegex='^---\n.*\:\ (.*)$';
+  switch (obj.charAt(0)) {
+	  // JSON
+    case '{':
+    case '[':
+      var jsonObj=JSON.parse(obj);
+      for (var key in jsonObj) { valueToCheck = jsonObj[key]; }
+      return valueToCheck;
+    // XML
+    case '<':
+      valueToCheck=obj.match(xmlRegex)[1];
+      return valueToCheck;
+    // YAML
+    case '-':
+      valueToCheck=obj.match(yamlRegex)[1];
+      return valueToCheck;
+    default:
+      errorFound=true;
+      errors.push("ERROR: Inputs unexpected!, please provide an object notation (arg). Inputs variables list (args[]) is deprecated.");
+  }
+}
+
+// recursive function to search value of specific key in subset defined
+function findObjectKeys(subset, searchKey) {
   if (searches.hasOwnProperty(searchKey) === false) {
     searches[searchKey] = "ERROR: not found";
   }
-  for (var item in mds) {
+  for (var item in subset) {
     // check if the key has a value or points to an object
-    if  (typeof (mds[item]) === "object") {
+    if  (typeof (subset[item]) === "object") {
       // if value is an object call recursively the function to search this subset of the object
-      findObjectKeys (mds[item], searchKey);
-    }
-    else{
+      findObjectKeys (subset[item], searchKey);
+    } else {
       // check if the key equals to the search term
       if (item === searchKey ) {
-        searches[searchKey] = mds[item];
+        searches[searchKey] = subset[item];
         keysWithSameName = keysWithSameName + 1;
         break;
       }
     }
   }
-}
-
-// call the function to find the keyName and return the value
-if (args[0]!=null) {
-  findObjectKeys(metadataset, args[0]);
-}
-else {
-  return "ERROR: no keyName provided";
-}
-
-// return the result or error message
-if (keysWithSameName > 1) {
-    return "ERROR: multiple keyNames found";
-}
-else if (keysWithSameName == 1) {
-    return searches[args[0]];
-}
-else  {
-    return "ERROR: keyname not found";
 }
